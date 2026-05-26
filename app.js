8c2477f4a30d5cd5f771024257a76d917ffaa027
CodyMarsee <cmarsee@alumni.iu.edu>
2026-05-26 10:58:52 -0400

    ENV

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 // const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
 const errorHandler = require("./middlewares/error-handler");
